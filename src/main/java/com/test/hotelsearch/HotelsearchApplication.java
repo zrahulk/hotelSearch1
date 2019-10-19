40cffcdef2e7485b466e0e16f8992e656256dda3
Nitish Bangera <nitish.bangera@sol.tech>
2019-10-19 23:25:54 +0530

    feat: Setting up HotelSearch App.

---
+++ src/main/java/com/test/hotelsearch/HotelsearchApplication.java
@@ -0,0 +1,13 @@
+package com.test.hotelsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsearchApplication.class, args);
+	}
+
+}
